92172670e9d098fc16bb70ee5a5b2fe6eef89738
ShahandFahad <shahandfahadmad@gmail.com>
2023-12-24 16:52:23 +0500

    Initial folder structure setup

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MainLayout() {
+  return (
+    <div>
+      <h1 class="text-xl font-bold underline">Solar Potential Analysis Tool</h1>
+      <></>
+    </div>
+  );
+}
