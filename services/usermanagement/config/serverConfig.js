3490059dfd42d49b6f39ac9a00127d847cbff307
ShahandFahad <dev.shahfahad@gmail.com>
2024-02-09 11:30:33 +0500

    Global synchronous handler

--- services/usermanagement/config/serverConfig.js
+++ services/usermanagement/config/serverConfig.js
@@ -1,3 +1,16 @@
+/* GLOBAL SYNCHRONOUS ERROR HANDLER */
+// Anywhere in th application, When a synchronous code exception is not handled
+// This block of code will handle it.
+// Using event listener
+process.on("uncaughtException", (error) => {
+  console.log("UNCAUGHT EXCEPTION (Synchronous): 🔥 Shutting down...");
+  console.log("Error name: ", err.name, " : Error Message: ", err.message);
+
+  // Shutdown the application gracefully
+  // Shut down the application: .exit(0) -> success : .exit(1) -> unhandled exception
+  process.exit(1); // Shutdown application
+});
+
 // Load Environment Variables: At the top of your app.js file
 require("dotenv").config({ path: `${__dirname}/env/.env.development` });
 // import app.js here
