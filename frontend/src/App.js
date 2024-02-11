d58c372b596a59ee8e8fd3ec925d97d9aad3b88f
ShahandFahad <dev.shahfahad@gmail.com>
2024-02-11 21:59:07 +0500

    Removed Unauthorized Routing and Added Authorized Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import Admin from "./admin/Admin";
-import Login from "./components/Auth/Login";
-import Signup from "./components/Auth/Signup";
-import MainLayout from "./layouts/MainLayout";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthProvider from "./provider/authProvider";
+import Routes from "./routes";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          {/* Login Page */}
-          <Route path="/login" element={<Login />} />
-          {/* Registeration Page */}
-          <Route path="/signup" element={<Signup />} />
-          {/* Client Side */}
-          <Route path="/*" element={<MainLayout />} />
-          {/* Admin Panel */}
-          <Route path="/admin/*" element={<Admin />} />
-        </Routes>
-      </Router>
-    </>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
 
 export default App;
+
+// The Routing Strategy Without Authentication
+// <>
+//   <Router>
+//     <Routes>
+//       {/* Login Page */}
+//       <Route path="/login" element={<Login />} />
+//       {/* Registeration Page */}
+//       <Route path="/signup" element={<Signup />} />
+//       {/* Client Side */}
+//       <Route path="/*" element={<MainLayout />} />
+//       {/* Admin Panel */}
+//       <Route path="/admin/*" element={<Admin />} />
+//     </Routes>
+//   </Router>
+// </>
