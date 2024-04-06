7a5b0b3950b097f11181352f57ffd316c25e6eb3
ShahandFahad <dev.shahfahad@gmail.com>
2024-04-06 18:00:25 +0500

    Removed Unused SolarCalc Service

--- services/solarcalculation/config/serverConfig.js
+++
@@ -1,9 +0,0 @@
-const app = require("../app");
-
-// Start server
-const PORT = 8002;
-app.listen(PORT, () => {
-  console.log(
-    `Solar Calculation: Server is running on: http://localhost:${PORT}`
-  );
-});
