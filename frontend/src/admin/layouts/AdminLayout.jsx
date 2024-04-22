af5f5e103306b271f68fd633a162d83534e7a43d
ShahandFahad <dev.shahfahad@gmail.com>
2024-04-22 22:32:00 +0500

    Removed Unused Compoenent

--- frontend/src/admin/layouts/AdminLayout.jsx
+++ frontend/src/admin/layouts/AdminLayout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Dashboard from "../pages/Dashboard";
-import Analytics from "../pages/Analytics";
 import Users from "../pages/Users";
 import Settings from "../pages/Settings";
 
@@ -13,7 +12,7 @@ export default function MainLayout() {
       {/* Navbar: Contains: Top Navbar and Sidebar */}
       <Navbar />
       {/* Basic Routes need to authorized */}
-      <div className="mt-14">
+      <div className="mt-12">
         {/* 
 
            All Admin Panel Routes
@@ -27,7 +26,7 @@ export default function MainLayout() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           {/* <Route path="dashboard/" element={<Dashboard />} /> */}
-          <Route path="analytics" element={<Analytics />} />
+          {/* <Route path="analytics" element={<Analytics />} /> */}
           <Route path="users" element={<Users />} />
           <Route path="settings" element={<Settings />} />
         </Routes>
