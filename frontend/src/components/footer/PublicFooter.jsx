04e9b5f63f6d9717fb6c2cc27ab53cec246a9bbd
ShahandFahad <dev.shahfahad@gmail.com>
2024-08-30 11:52:25 +0500

    Updated class to className

--- frontend/src/components/footer/PublicFooter.jsx
+++ frontend/src/components/footer/PublicFooter.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import logo from "../../assets/images/logo.png";
 import "./footerstyle.css";
-import { Link } from "react-router-dom";
 export default function PublicFooter() {
   return (
-    <div class="footer-wrapper border-gradient-top">
-      <div class="footer-title-link">
-        <img alt="logo" class="footer-logo" src={logo} />
+    <div className="footer-wrapper border-gradient-top">
+      <div className="footer-title-link">
+        <img alt="logo" className="footer-logo" src={logo} />
       </div>
-      <div class="footer-links">
-        <span class="footer-link">CUIST FYP, All Rights Reserved 2024.</span>
+      <div className="footer-links">
+        <span className="footer-link">
+          CUIST FYP, All Rights Reserved 2024.
+        </span>
         {/* 
-        <a class="footer-link" href="/about-us">
+        <a className="footer-link" href="/about-us">
           About
         </a>
-        <a class="footer-link" href="/about-us">
+        <a className="footer-link" href="/about-us">
           Contact
         </a>
-        <a class="footer-link" href="/login">
+        <a className="footer-link" href="/login">
           Login
         </a>
-        <a class="footer-link" href="/signup">
+        <a className="footer-link" href="/signup">
           Signup
         </a>
-        <button class="footer-link btn-text mobile-hide">
-          <a class="footer-link" href="/about-us">
+        <button className="footer-link btn-text mobile-hide">
+          <a className="footer-link" href="/about-us">
             Send feedback
           </a>
         </button>
