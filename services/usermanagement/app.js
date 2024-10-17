3e8b15474007cfb0ce7e743bf33c8c75e9aea598
ShahandFahad <dev.shahfahad@gmail.com>
2024-10-17 07:51:23 +0500

    Added security parameters

--- services/usermanagement/app.js
+++ services/usermanagement/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
-var cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanatize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+
+// Routes
+const userRoutes = require("./src/routes/userRoutes");
 
 // Utilities
 const AppError = require("./src/utils/appError"); // Custom error handler
@@ -8,19 +16,39 @@ const globalErrorHandler = require("./src/controllers/errorController"); // Hand
 // APP
 const app = express();
 
-app.use(cors()); // allowing cross-origin
-app.use(express.json()); // json parser: req handler middleware
+// allowing cross-origin
+app.use(cors());
+
+// HTTP header protection
+app.use(helmet());
 
 // Run in development env
 if (process.env.NODE_ENV === "development") {
-  const morgan = require("morgan");
-  app.use(morgan("dev"));
+    const morgan = require("morgan");
+    app.use(morgan("dev"));
 }
 
-//  User routes
-const userRoutes = require("./src/routes/userRoutes");
+// Limit the number of request from same ip up to 100 per hour
+const limmiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many request - Please try again in an hour!"
+});
+app.use("/api", limmiter);
 
-// Routes
+// Data sanatization against noSql query injection
+app.use(mongoSanatize());
+
+// Data sanatization against XSS (cross site scripting)
+app.use(xss());
+
+// Prevent paramter pollution : Clean query string
+// app.use(hpp({whitelist: ["define your white listed query parameters here"]}));
+
+// body parser, limit payload
+app.use(express.json({ limit: "10kb" }));
+
+// End points 
 app.use("/api/v1/user", userRoutes);
 
 // NON EXISTENT, UNHANDLED ROUTES
@@ -31,8 +59,8 @@ app.use("/api/v1/user", userRoutes);
 // Use middleware
 
 app.use("*", (req, res, next) => {
-  // Pass error to the next middleware (next middleware after this is the global error handler)
-  next(new AppError(`Cann't find ${req.originalUrl} on this server!`, 404));
+    // Pass error to the next middleware (next middleware after this is the global error handler)
+    next(new AppError(`Cann't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Call Global Error Handler Middleware
